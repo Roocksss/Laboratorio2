05e9938b35194d1b982d3cc579b28998588dd6ec
Sergio Rock <rock.sergio.a@gmail.com>
2025-05-26 15:23:26 -0600

    Primer commit RSA

--- RSA.py
+++ RSA.py
@@ -2,4 +2,33 @@
 import numpy as np 
 import random 
 
+def main(): 
+    try:
+        p = int(input("Ingresa un número primo p: "))
+        q = int(input("Ingresa otro númrero primo q: "))
+    
+    except ValueError: 
+        print("Ingresa un número primo válido")
+    
+    else: 
+        print("Perfecto")
+
+        n = p*q
+        On = (p-1)*(q-1)
+        e = random.randint(2,On-1)
+        while np.gcd(e,On) != 1:
+            e = random.randint(2,On-1)
+        d = pow(e, -1,On)
+        #Generación de la llaves 
+        #Llave pública
+        llave_publica = (n,e)
+        #Llave privada 
+        llave_privada = (n,d)
+        print("Llave pública: ", llave_publica)
+        print("Llave privada: ", llave_privada)
+
+def cifrar(msj, llave_publica): 
+    n, e = llave_publica
+    msj_cifrado =            
+main() 
 
