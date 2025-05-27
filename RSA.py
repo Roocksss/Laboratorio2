71bdd83c6b509545df1a09cda02c829f5d2c9188
Sergio Rock <rock.sergio.a@gmail.com>
2025-05-27 17:18:26 -0600

    Funciones completadas, falta unión

--- RSA.py
+++ RSA.py
@@ -53,11 +53,49 @@ def descifrar(C,llave_privada):
     return M 
 
 def euclides(a, b): 
-    while b != 0:
-        a, b = b, a % b
-    return a
+    if a == 0: 
+        return b
+    else: 
+        return euclides(b % a, a)
 
+def inv_mult(A,N):
 
+    def egcd(a,b): 
+        if a == 0: 
+            return b,0,1
+        
+        g,x1,y1 = egcd(b % a, a)
+        x = y1 - (b // a) * x1
+        y = x1 
 
+        return g,x,y
+    
+    g,x,y = egcd(A % N, N)
+
+    if g != 1: 
+        return None 
+
+    return (x % N + N) % N  #Positivo
+
+def expon_mod(a,k,N): 
+    b=1 
+    if k == 0:
+        return b
+
+    A = a % N
+
+    if k & 1:   #Bit a bit 
+        b = A % N 
+
+    K = k >> 1
+
+    while K > 0:
+        A = (A * A) % N
+        if K & 1:  #Bit a bit
+            b = (b * A) % N
+        
+        K >>= 1
+    
+    return b
 
 
